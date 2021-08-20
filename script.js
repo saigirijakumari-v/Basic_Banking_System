6b10c5e9d5c29f3d52e462326a51b6b22aed3c4e
saigirijakumari-v <86467422+saigirijakumari-v@users.noreply.github.com>
2021-08-20 20:11:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-var menu = document.getElementById('menu');
-var menu_bar = document.getElementById('menu-bar');
-var btns = menu_bar.getElementsByClassName('btn');
-menu.addEventListener("click", function () {
-    menu.classList.toggle("active");
-    menu_bar.classList.toggle("active");
-    })
-for(var i=0;i<btns.length;i++){
-    btns[i].addEventListener("click",function(){
-        var current= document.getElementsByClassName("selected");
-        current[0].className.replace("selected", "");
-        this.className += "selected";
-    });
-}
+ var menu = document.getElementById('menu');
+ var menu_bar = document.getElementById('menu-bar');
+ menu.addEventListener("click", function () {
+     menu.classList.toggle("active");
+     menu_bar.classList.toggle("active");
+     });
